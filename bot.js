a61275b5c989050e4f56a920d4cd2d2551b3a7c1
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-06 17:36:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,21 +100,3 @@ client.on("guildMemberAdd", member => {
     }).catch(console.error)
 })
 
-
-
-//تبنيد الوهمي
-
-const moment = require("moment")
-client.on("guildMemberAdd", m => {
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-        m.ban();
-    };
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    };
-    
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    };
-});
