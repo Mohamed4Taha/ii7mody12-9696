0d532b922f9527be21ca4616e7420d34dff73a86
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-08 22:11:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,20 +102,3 @@ client.on("guildMemberAdd", member => {
 
        
  
-       
- //رسالة عالخاص
-       
- client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTimestamp()
-            .setTitle('رساله جديده في خاص البوت')
-            .setThumbnail(${message.author.avatarURL})
-            .setDescription(\n\n`${message.content}
-)
-            .setFooter(من (@${message.author.tag})  |  (${message.author.id}))
-        client.channels.get("509678403100737537").send({ embed: stewart });
-    }
-});
